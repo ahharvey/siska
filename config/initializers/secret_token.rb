cc968643b6df3b917fa32a16eeff878119b8c221
Andrew Harvey <ah.harvey@gmail.com>
2011-11-15 18:48:49 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siska::Application.config.secret_token = '8cba5e5ebff924c0556c03a934fcb3fcb0ec58672292487dfe4d2d4cd28000f067d998bde3f79910dbd405be40c8fec9674c0d152590273952a02f2bc1357b52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Siska::Application.config.session_store :cookie_store, :key => '_siska_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Siska::Application.config.session_store :active_record_store
